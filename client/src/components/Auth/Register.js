86b92f9941e90ebc1ca743959e316d0814efc81e
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-02-29 23:26:47 -0600

    Register component with validation. Will start on Diner view component tomorrow

--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Form, Input, Label } from 'reactstrap';
+import { Button, Form, Input, Label, Alert } from 'reactstrap';
 import axios from 'axios';
 
 function Register(props) {
@@ -9,7 +9,7 @@ function Register(props) {
         username: '',
         password: '',
         email: '',
-        role: ''
+        role: 'operator'
     });
 
     /* error and success state for validation */
@@ -28,18 +28,46 @@ function Register(props) {
     const register = (user) => {
 
         /* validation */
+        if (user.username === '' || user.password === '' || user.email === '') {
+            setErr('You must fill out all of the fields to register.');
+            return;
+        }
+        else if (user.username.length < 5 || user.username.length > 254) {
+            setErr('Please make sure that your username is at least 5 characters long.');
+            return;
+        }
+        else if (user.email.length < 5 || user.email.length > 254) {
+            setErr('Please enter a valid email address.');
+            return;
+        }
+        else if (user.password.length < 8 || user.password.length > 128) {
+            setErr('Please enter a password that is at least 8 characters long.');
+            return;
+        }
+        else if (user.username.match(/[^a-z0-9]/gi, '')) {
+            setErr('Please enter a valid username that containts only alphanumeric characters.');
+            return;
+        }
+        else if (!user.email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+            setErr('Please enter a valid email address.');
+            return;
+        }
+        else {
 
+            console.log(user);
+            setSuccess('Registering...');
 
-        /* temp filler request */
-        axios.post('/api/auth/register', user)
-            .then(res => {
-                console.log(res);
-                /* dispatch */
-            })
-            .catch(err => {
-                console.log(err);
-                /* dispatch */
-            })
+            /* temp filler request */
+            // axios.post('/api/auth/register', user)
+            //     .then(res => {
+            //         console.log(res);
+            //         /* dispatch */
+            //     })
+            //     .catch(err => {
+            //         console.log(err);
+            //         /* dispatch */
+            //     })
+        }
     }
 
     return (
@@ -48,16 +76,43 @@ function Register(props) {
                 e.preventDefault();
                 register(user);
             }}>
-                <Input name="username" placeholder="Username" value={user.username} autoComplete="off" />
-                <Input name="email" placeholder="Email" value={user.email} autoComplete="off" />
-                <Input name="password" placeholder="Password" value={user.password} autoComplete="off" />
+                <Input
+                    name="username"
+                    placeholder="Username"
+                    value={user.username}
+                    autoComplete="off"
+                    onChange={handleChange}
+                />
+                <Input
+                    name="email"
+                    placeholder="Email"
+                    value={user.email}
+                    autoComplete="off"
+                    onChange={handleChange}
+                />
+                <Input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={user.password}
+                    autoComplete="off"
+                    onChange={handleChange}
+                />
                 <Label htmlFor="role">Role:</Label>
-                <Input type="select" name="role" id="role">
-                    <option value={user.role}>Operator</option>
-                    <option value={user.role}>Diner</option>
+                <Input type="select" name="role" id="role" onChange={handleChange} value={user.role}>
+                    <option value="operator" name="operator">Operator</option>
+                    <option value="diner" name="diner">Diner</option>
                 </Input>
                 <Button type="submit" className="register-btn">Register</Button>
             </Form>
+            {err &&
+                <Alert style={{ marginTop: '10px' }} color="danger">
+                    {err}
+                </Alert>}
+            {success &&
+                <Alert style={{ marginTop: '10px' }} color="success">
+                    {success}
+                </Alert>}
         </div>
     )
 }
